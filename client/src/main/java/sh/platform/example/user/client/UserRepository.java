b740b4b06b8dde5a4a83a30030759600ea93248d
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-03 13:51:26 +0000

    updates the user file

--- client/src/main/java/sh/platform/example/user/client/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package sh.platform.example.user.client;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, String> {
-
-}
